f8afdaf27ad24470f9f8e3f4aba4b52a1ad22683
kjun <keisoft.kjun@gmail.com>
2024-09-30 20:12:41 +0900

    ..

--- OllamaWinFormsApp/Form1.cs
+++ OllamaWinFormsApp/Form1.cs
@@ -15,13 +15,11 @@ namespace OllamaWinFormsApp
 
         private async void Test()
         {
-            // set up the client
             var uri = new Uri("http://localhost:11434");
             var ollama = new OllamaApiClient(uri);
 
             var models = await ollama.ListLocalModels();
 
-            // select a model which should be used for further operations
             ollama.SelectedModel = "llama3.2:latest";
 
             await foreach (var stream in ollama.Generate("서울의 수도는?"))
